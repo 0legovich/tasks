d14bebae028ef64f6e79f0d12a966fab5a935892
Shpak Roman <romq1@mail.ru>
2019-06-30 20:14:31 +0300

    Updates

---
+++ precache-manifest.358d265e00a3bbfb1b9355a476d39e27.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b27bb03c6e39b23be115103547149ca",
+    "url": "/tasks/index.html"
+  },
+  {
+    "revision": "1277d7bea1938b58f504",
+    "url": "/tasks/static/css/main.c8fb1244.chunk.css"
+  },
+  {
+    "revision": "7b4ccd60c27dcbf56a1a",
+    "url": "/tasks/static/js/2.0e1f8434.chunk.js"
+  },
+  {
+    "revision": "1277d7bea1938b58f504",
+    "url": "/tasks/static/js/main.0841f8e5.chunk.js"
+  },
+  {
+    "revision": "7d8089a231448516824e",
+    "url": "/tasks/static/js/runtime~main.b1405a7c.js"
+  }
+]);
